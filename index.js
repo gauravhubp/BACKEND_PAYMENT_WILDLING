260a2f84a3238fefa2871b831bd2af62dabae7f6
Gaurav <gauravpatel3521@gmail.com>
2023-06-27 22:16:46 +0530

    dsdsdsd

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // This example sets up an endpoint using the Express framework.
 // Watch this video to get started: https://youtu.be/rPR2aJ6XnAc.
-
-const express = require('express');
+const express=require("express")
 const app = express();
 const stripe = require('stripe')('sk_test_51NHPWGSAcfclqU9cSd5svYvzbHgh95kpn09wqMuPngVzy6iFkXIJyTFwPDfefLFIK3iZczrejWdrcOySaw36BdL500enej6SUd')
-const express=require("express")
+
+const cors = require("cors");
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
