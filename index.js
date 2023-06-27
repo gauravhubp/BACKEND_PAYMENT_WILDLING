4e524c3e2ae3b52e8fdea05162e04a2ce788e406
Gaurav <gauravpatel3521@gmail.com>
2023-06-27 13:32:00 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// This example sets up an endpoint using the Express framework.
+// Watch this video to get started: https://youtu.be/rPR2aJ6XnAc.
+
+const express = require('express');
+const app = express();
+const stripe = require('stripe')('sk_test_51NHPWGSAcfclqU9cSd5svYvzbHgh95kpn09wqMuPngVzy6iFkXIJyTFwPDfefLFIK3iZczrejWdrcOySaw36BdL500enej6SUd')
+
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: 'inr',
+          product_data: {
+            name: 'T-shirt',
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: 'http://localhost:4242/success',
+    cancel_url: 'http://localhost:4242/cancel',
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.listen(4242, () => console.log(`Listening on port ${4242}!`));
