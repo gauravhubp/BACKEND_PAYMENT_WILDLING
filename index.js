b5669ecba6d2284e9b503377d49b291c6d92cc86
Gaurav <gauravpatel3521@gmail.com>
2023-06-27 17:16:15 +0530

    final

--- index.js
+++ index.js
@@ -1,30 +1,67 @@
-// This example sets up an endpoint using the Express framework.
-// Watch this video to get started: https://youtu.be/rPR2aJ6XnAc.
+require('dotenv').config()
 
-const express = require('express');
-const app = express();
-const stripe = require('stripe')('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')
+const app = require("express")();
+const path = require("path");
 
-app.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: 'inr',
-          product_data: {
-            name: 'T-shirt',
-          },
-          unit_amount: 2000,
-        },
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: 'http://localhost:4242/success',
-    cancel_url: 'http://localhost:4242/cancel',
-  });
+const cors = require("cors");
 
-  res.redirect(303, session.url);
+const shortid = require("shortid");
+const Razorpay = require("razorpay");
+
+const express=require("express")
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors())
+let price=0;
+
+app.post("/",async(req,res) =>
+{
+const cart=req.body.cart;
+price=cart.map(item => item.price*item.quantity).reduce((total,value) => total+value,0)
+console.log(price);
+})
+
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
-app.listen(4242 || process.env.PORT, () => console.log(`Listening on port !`));
+app.use(cors());
+
+// Serving company logo
+app.get("/logo.png", (req, res) => {
+  res.sendFile(path.join(__dirname, "logo.png"));
+});
+
+
+
+app.post("/razorpay", async (req, res) => {
+  const payment_capture = 1;
+  const amount = price;
+  const currency = "INR";
+
+
+  const options = {
+    amount: amount * 100,
+    currency,
+    receipt: shortid.generate(),
+    payment_capture,
+  };
+
+  try {
+    const response = await razorpay.orders.create(options);
+    console.log(response);
+    res.json({
+      id: response.id,
+      currency: response.currency,
+      amount: response.amount,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(1337 || process.env.PORT, () => {
+  console.log("Backend running at localhost:1337");
+});
